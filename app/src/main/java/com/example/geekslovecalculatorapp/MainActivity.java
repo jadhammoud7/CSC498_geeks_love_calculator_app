be4675557895e4e1aa5b2f3901a3684fb509e41a
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-02-13 17:54:32 +0200

    added wallpaper and edittext

--- app/src/main/java/com/example/geekslovecalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/geekslovecalculatorapp/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.geekslovecalculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText full_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        full_name=(EditText)findViewById(R.id.full_name);
+    }
+    public void name(View v){
+        String s="";
+        full_name.setText(s);
     }
 }
