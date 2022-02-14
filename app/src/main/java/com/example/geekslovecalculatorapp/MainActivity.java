22b11029a4787b849c4421d0e857df2dbb1a4016
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-02-14 02:43:58 +0200

    added spinner and textfield

--- app/src/main/java/com/example/geekslovecalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/geekslovecalculatorapp/MainActivity.java
@@ -4,18 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     EditText full_name;
+    String[] languages = { "C", "Java","Python", "HTML","MYSQL", "C#" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         full_name=(EditText)findViewById(R.id.full_name);
+        //
+        Spinner spino = findViewById(R.id.dropdownlist);
+        spino.setOnItemSelectedListener(this);
+        ArrayAdapter ad = new ArrayAdapter(this,android.R.layout.simple_spinner_item,languages);
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spino.setAdapter(ad);
+
     }
     public void name(View v){
         String s="";
         full_name.setText(s);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
